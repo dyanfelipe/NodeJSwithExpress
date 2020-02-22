2efff7a7659a1ac09851a640ad467602af42ec1f
dyanfelipe <dyansilva@outlook.com>
2020-02-22 14:35:35 -0300

    test  to save user with models

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
-import dotenv from 'dotenv';
+import './database';
+
 dotenv.config();
+
 class App {
   constructor() {
     this.server = express();
