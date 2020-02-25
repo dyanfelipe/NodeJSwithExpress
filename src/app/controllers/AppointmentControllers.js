c87767afc44a2b5e1e461da8b92671eaef1b894d
dyanfelipe <dyansilva@outlook.com>
2020-02-25 10:26:07 -0300

    list scheduling of users

--- src/app/controllers/AppointmentControllers.js
+++ src/app/controllers/AppointmentControllers.js
@@ -1,9 +1,31 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointments';
+import File from '../models/File';
 import User from '../models/User';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      onder: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        },
+      ],
+    });
+    return res.status(200).json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
--- src/routes.js
+++ src/routes.js
@@ -15,11 +15,12 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionControllers.store);
-// routas que precesam de auth
+// rotas que precesam de auth
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProvidersController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 export default routes;
